ce507d58062053f8c9eb23eff5d2eb1086eb39c2
Richard <rschneider1st@gmail.com>
2021-02-16 01:08:54 -0600

    provide graphing coordinates for eigenvectors

--- Graph-Clustering/Graph.h
+++ Graph-Clustering/Graph.h
@@ -59,11 +59,45 @@ public:
 			infile.close();
 		}
 	}
-	LinAlg::Vector EigenClustering(int k) {
+	std::pair<LinAlg::Matrix, LinAlg::Vector> EigenClustering(int k) {
 		/* This function takes the adj matrix, finds the 
 		eigenvectors, and clusters the graph into k parts*/
-		LinAlg::Matrix eigenvects = adj.Eigenvectors();
-		return eigenvects.KMeans(k);
+		// find eigenvectors and cluster vectors
+		std::pair<LinAlg::Matrix, LinAlg::Matrix> eig_pair = adj.QRAlgorithm();
+		LinAlg::Matrix eigenvects = eig_pair.first;
+		LinAlg::Matrix eigenval = eig_pair.second;
+		LinAlg::Vector clusters = eigenvects.KMeans(k);
+
+		// find two largest eigenvalues and make coordinate matrix for output
+		double max1 = eigenval[0][0];
+		double max2 = eigenval[1][1];
+		int loc1 = 0;
+		int loc2 = 1;
+		if (max2 > max1) {
+			std::swap(max1, max2);
+			std::swap(loc1, loc2);
+		}
+		for (int i = 2; i < eigenval[0].size(); i++) {
+			if (max1 < eigenval[i][i]) {
+				loc1 = i;
+				max1 = eigenval[i][i];
+				loc2 = loc1;
+				max2 = max1;
+			}
+			else if (max2 < eigenval[i][i]) {
+				max2 = eigenval[i][i];
+				loc2 = i;
+			}
+		}
+		LinAlg::Matrix coord(std::vector<LinAlg::Vector>{eigenvects.GetCol(loc1), eigenvects.GetCol(loc2)});
+
+		// get vector of real eigenvalues
+		LinAlg::Vector eig(eigenval[0].size(), 0);
+#pragma omp parallel for
+		for (int i = 0; i < eigenval[0].size(); i++) {
+			eig[i] = eigenval[i][i];
+		}
+		return std::make_pair(coord, eig);
 	}
 };
 
